1fc05ea21067f2c3f9c2a05c90972e3fe5cb349a
ozgurozbekuk <ozgurozbek.uk@gmail.com>
2023-07-02 13:23:46 +0300

    search-filter
    Search filter with React.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import Data from './Data.json'
+import './App.css'
 
 function App() {
+
+  const [searchTerm,setSearchTerm] =useState("")
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Kurtlar Vadisi Konsey Üyeleri</h1>
+      <div className='inputContainer'>
+        <input className='search' type='text' placeholder='Konsey Üyesi giriniz...' onChange={(e) => {
+          setSearchTerm(e.target.value)
+        }}/>
+      </div>
+      <div className='dataContainer'>
+        {
+          Data.filter((val) => {
+            if(searchTerm =="") {
+              return val;
+            }else if(val.title.toLocaleLowerCase().includes(searchTerm.toLocaleLowerCase())) {
+              return val;
+            }
+          }).map((val) => {
+            return(<div className='data' key={val.id}>
+                <img src={val.image}/>
+                <h3>{val.title}</h3>
+                <p>{val.Description}</p>
+            </div>)
+          })
+        }
+      </div>
     </div>
   );
 }
